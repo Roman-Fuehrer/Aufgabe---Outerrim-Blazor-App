429489b189a072541beff91b84f65c94d33a9ab4
Roman-Fuehrer <r.fuehrer@htlkrems.at>
2025-02-19 08:40:38 +0100

    volles Risiko

--- Aircrafts/OuterrimAirship/Model/Machinery.cs
+++ Aircrafts/OuterrimAirship/Model/Machinery.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OuterrimAirship.Model;
@@ -5,5 +6,18 @@ namespace OuterrimAirship.Model;
 [Table("Machineries")]
 public class Machinery
 {
+    #region Properties
+    
+    [Column("Machinery_Id"),Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required,Column("Lable")]
+    public string Label { get; set; }
+    
+    [Required, Column("Funktion")]
+    public string Text { get; set; }
+    
+    #endregion
+    
     
 }
